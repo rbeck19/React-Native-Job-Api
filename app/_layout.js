419d767ed9a04b212bbc6d0fa6dbc5e5b02b5f1e
rbeck19 <ryan.beck219@gmail.com>
2024-08-28 12:34:36 -0400

    part 1 of home page

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,40 @@
 import { Stack } from "expo-router";
+import { useCallback, useEffect } from "react";
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function RootLayout() {
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  })
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if(fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  //   console.log("in callback")
+  // },[fontsLoaded])
+
+  useEffect(() => {
+    const hideSplashScreen = async () => {
+      if (fontsLoaded) {
+        await SplashScreen.hideAsync();
+        console.log("Splash screen hidden");
+      }
+    };
+
+    hideSplashScreen();
+  }, [fontsLoaded]);
+
+  if(!fontsLoaded) return null;
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Stack  />
   );
 }
+
+export default Layout;
